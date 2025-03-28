24908a02405c95e268dd4c3c495f0ff33c7b5709
Daniel-PC <danielnoam1999@gmail.com>
2025-03-28 16:34:41 +0300

    Update

--- Assets/5_Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/5_Assets/QuickOutline/Scripts/Outline.cs
@@ -13,7 +13,8 @@ using UnityEngine;
 
 [DisallowMultipleComponent]
 
-public class Outline : MonoBehaviour {
+public class Outline : MonoBehaviour { 
+  
   private static HashSet<Mesh> registeredMeshes = new HashSet<Mesh>();
 
   public enum Mode {
